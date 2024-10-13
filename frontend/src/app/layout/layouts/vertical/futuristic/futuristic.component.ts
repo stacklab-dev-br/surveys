bbc7c6f4bd2d0693249b1d5519cefeb5724caf08
Mateus Uliana <mateus@uliana.dev.br>
2024-10-13 01:00:50 -0300

    Remove componentes inúteis

--- frontend/src/app/layout/layouts/vertical/futuristic/futuristic.component.ts
+++ frontend/src/app/layout/layouts/vertical/futuristic/futuristic.component.ts
@@ -14,11 +14,7 @@ import { Navigation } from 'app/core/navigation/navigation.types';
 import { UserService } from 'app/core/user/user.service';
 import { User } from 'app/core/user/user.types';
 import { LanguagesComponent } from 'app/layout/common/languages/languages.component';
-import { MessagesComponent } from 'app/layout/common/messages/messages.component';
 import { NotificationsComponent } from 'app/layout/common/notifications/notifications.component';
-import { QuickChatComponent } from 'app/layout/common/quick-chat/quick-chat.component';
-import { SearchComponent } from 'app/layout/common/search/search.component';
-import { ShortcutsComponent } from 'app/layout/common/shortcuts/shortcuts.component';
 import { UserComponent } from 'app/layout/common/user/user.component';
 import { Subject, takeUntil } from 'rxjs';
 
@@ -35,12 +31,8 @@ import { Subject, takeUntil } from 'rxjs';
         MatIconModule,
         LanguagesComponent,
         FuseFullscreenComponent,
-        SearchComponent,
-        ShortcutsComponent,
-        MessagesComponent,
         NotificationsComponent,
         RouterOutlet,
-        QuickChatComponent,
     ],
 })
 export class FuturisticLayoutComponent implements OnInit, OnDestroy {
